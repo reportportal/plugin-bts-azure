af4e667fd1804b222e33a36163dc86278fe75b54
Alexandr Kislov <alexandr_kislov@epam.com>
2021-07-07 12:17:25 +0300

    EPMRPP-62673: add integration details display

--- ui/src/components/azureIntegrationSettings/azureIntegrationSettings.jsx
+++ ui/src/components/azureIntegrationSettings/azureIntegrationSettings.jsx
@@ -1,3 +1,10 @@
+import { BtsAuthFieldsInfo } from 'components/btsAuthFieldsInfo';
+import {
+  URL_ATTRIBUTE_KEY,
+  PROJECT_ATTRIBUTE_KEY,
+  USERNAME_ATTRIBUTE_KEY,
+  CHECKBOX_ATTRIBUTE_KEY,
+} from 'components/constants';
 import { AzureIntegrationFormFields } from '../azureIntegrationFormFields';
 
 export const AzureIntegrationSettings = (props) => {
@@ -7,11 +14,37 @@ export const AzureIntegrationSettings = (props) => {
     components: { IntegrationSettings: IntegrationSettingsContainer },
   } = extensionProps;
 
+  const authFieldsConfig = [
+    {
+      value: data.integrationParameters[URL_ATTRIBUTE_KEY],
+      message: 'Link to BTS',
+    },
+    {
+      value: data.integrationParameters[PROJECT_ATTRIBUTE_KEY],
+      message: 'Project Name',
+    },
+
+    data.integrationParameters[USERNAME_ATTRIBUTE_KEY]
+      ? {
+          value: data.integrationParameters[USERNAME_ATTRIBUTE_KEY],
+          message: 'Authorized by',
+        }
+      : {
+          value: data.integrationParameters[CHECKBOX_ATTRIBUTE_KEY] ? 'Yes' : 'No',
+          message: 'Allow users to post issues using this Token',
+        },
+  ];
+
+  const getEditAuthConfig = () => ({
+    content: <BtsAuthFieldsInfo fieldsConfig={authFieldsConfig} />,
+  });
+
   return (
     <IntegrationSettingsContainer
       data={data}
       onUpdate={onUpdate}
       goToPreviousPage={goToPreviousPage}
+      editAuthConfig={getEditAuthConfig()}
       isGlobal={isGlobal}
       formFieldsComponent={(rest) => <AzureIntegrationFormFields {...extensionProps} {...rest} />}
     />
--- ui/src/components/constants.js
+++ ui/src/components/constants.js
@@ -1,3 +1,6 @@
+export const URL_ATTRIBUTE_KEY = 'url';
+export const PROJECT_ATTRIBUTE_KEY = 'project';
 export const USERNAME_ATTRIBUTE_KEY = 'username';
 export const PASSWORD_ATTRIBUTE_KEY = 'password';
 export const TOKEN_ATTRIBUTE_KEY = 'oauthAccessKey';
+export const CHECKBOX_ATTRIBUTE_KEY = 'postAllowed';
