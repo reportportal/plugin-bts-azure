b9d33c569f8da405687167c8fe096d814c928574
Alexandr Kislov <alexandr_kislov@epam.com>
2021-08-11 11:17:37 +0300

    EPMRPP-62677: Implement BtsExtension interface

--- src/main/java/com/epam/reportportal/extension/azure/AzureExtension.java
+++ src/main/java/com/epam/reportportal/extension/azure/AzureExtension.java
@@ -1,8 +1,22 @@
 package com.epam.reportportal.extension.azure;
 
+import com.epam.reportportal.extension.IntegrationGroupEnum;
 import com.epam.reportportal.extension.PluginCommand;
 import com.epam.reportportal.extension.ReportPortalExtensionPoint;
 import com.epam.reportportal.extension.azure.command.connection.TestConnectionCommand;
+import com.epam.reportportal.extension.azure.rest.client.ApiClient;
+import com.epam.reportportal.extension.azure.rest.client.ApiException;
+import com.epam.reportportal.extension.azure.rest.client.Configuration;
+import com.epam.reportportal.extension.azure.rest.client.api.ClassificationNodesApi;
+import com.epam.reportportal.extension.azure.rest.client.api.FieldsApi;
+import com.epam.reportportal.extension.azure.rest.client.api.WorkItemTypesApi;
+import com.epam.reportportal.extension.azure.rest.client.api.WorkItemTypesFieldApi;
+import com.epam.reportportal.extension.azure.rest.client.auth.HttpBasicAuth;
+import com.epam.reportportal.extension.azure.rest.client.model.workitem.WorkItemClassificationNode;
+import com.epam.reportportal.extension.azure.rest.client.model.workitem.WorkItemField;
+import com.epam.reportportal.extension.azure.rest.client.model.workitem.WorkItemType;
+import com.epam.reportportal.extension.azure.rest.client.model.workitem.WorkItemTypeFieldWithReferences;
+import com.epam.reportportal.extension.bugtracking.BtsExtension;
 import com.epam.reportportal.extension.common.IntegrationTypeProperties;
 import com.epam.reportportal.extension.event.PluginEvent;
 import com.epam.reportportal.extension.event.StartLaunchEvent;
@@ -23,13 +37,26 @@ import com.epam.ta.reportportal.dao.IntegrationRepository;
 import com.epam.ta.reportportal.dao.IntegrationTypeRepository;
 import com.epam.ta.reportportal.dao.LaunchRepository;
 import com.epam.ta.reportportal.dao.ProjectRepository;
+import com.epam.ta.reportportal.entity.integration.Integration;
+import com.epam.ta.reportportal.exception.ReportPortalException;
+import com.epam.ta.reportportal.ws.model.ErrorType;
+import com.epam.ta.reportportal.ws.model.externalsystem.AllowedValue;
+import com.epam.ta.reportportal.ws.model.externalsystem.PostFormField;
+import com.epam.ta.reportportal.ws.model.externalsystem.PostTicketRQ;
+import com.epam.ta.reportportal.ws.model.externalsystem.Ticket;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.MapperFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.introspect.JacksonAnnotationIntrospector;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 import org.jooq.DSLContext;
 import org.pf4j.Extension;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.DisposableBean;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
@@ -55,13 +82,21 @@ import java.util.stream.Stream;
  * @author <a href="mailto:ivan_budayeu@epam.com">Ivan Budayeu</a>
  */
 @Extension
-public class AzureExtension implements ReportPortalExtensionPoint, DisposableBean {
+public class AzureExtension implements ReportPortalExtensionPoint, DisposableBean, BtsExtension {
 
-	public static final String BINARY_DATA_PROPERTIES_FILE_ID = "azure-binary-data.properties";
+	public static final Logger LOGGER = LoggerFactory.getLogger(AzureExtension.class);
+
+    public static final String BINARY_DATA_PROPERTIES_FILE_ID = "azure-binary-data.properties";
 
 	public static final String SCHEMA_SCRIPTS_DIR = "schema";
 
 	private static final String PLUGIN_ID = "Azure";
+
+	private static final String API_VERSION = "6.0";
+
+	private static final String EXPAND = "All";
+
+	private static final Integer DEPTH = 15;
 
 	private final String resourcesDir;
 
@@ -138,6 +173,11 @@ public class AzureExtension implements ReportPortalExtensionPoint, DisposableBea
 		return pluginCommandMapping.get().get(commandName);
 	}
 
+	@Override
+	public IntegrationGroupEnum getIntegrationGroup() {
+		return IntegrationGroupEnum.BTS;
+	}
+
 	@PostConstruct
 	public void createIntegration() throws IOException {
 		initListeners();
@@ -184,4 +224,165 @@ public class AzureExtension implements ReportPortalExtensionPoint, DisposableBea
 		pluginCommandMapping.put("testConnection", new TestConnectionCommand());
 		return pluginCommandMapping;
 	}
+
+	@Override
+    // Never called method. Connection is tested via the command.
+	public boolean testConnection(Integration system) {
+		return false;
+	}
+
+	@Override
+    // TODO: Implement in the future
+	public Optional<Ticket> getTicket(String id, Integration system) {
+		return Optional.empty();
+	}
+
+    @Override
+    // TODO: Implement in the future
+    public Ticket submitTicket(PostTicketRQ ticketRQ, Integration system) {
+        return null;
+    }
+
+	@Override
+	public List<PostFormField> getTicketFields(String issueType, Integration integration) {
+		Map<String, Object> params = integration.getParams().getParams();
+		String organizationUrl = params.get("url").toString();
+		String projectName = params.get("project").toString();
+		String personalAccessToken = params.get("oauthAccessKey").toString();
+
+		ApiClient defaultClient = getConfiguredApiClient(personalAccessToken);
+		String organizationName = organizationUrl.replace(defaultClient.getBasePath(), "");
+
+		ClassificationNodesApi nodesApi = new ClassificationNodesApi(defaultClient);
+		List<WorkItemClassificationNode> areaNodes = new ArrayList<>();
+		List<WorkItemClassificationNode> iterationNodes = new ArrayList<>();
+		try {
+            List<WorkItemClassificationNode> nodes = nodesApi
+                    .classificationNodesGetRootNodes(organizationName, projectName, API_VERSION, DEPTH);
+			for (WorkItemClassificationNode node : nodes) {
+				if (node.getStructureType().equals("area")) {
+					areaNodes = extractNestedNodes(node);
+				} else if (node.getStructureType().equals("iteration")) {
+					iterationNodes = extractNestedNodes(node);
+				}
+			}
+		} catch (ApiException e) {
+            LOGGER.error("Unable to load classification nodes: " + e.getMessage(), e);
+			throw new ReportPortalException(ErrorType.UNABLE_INTERACT_WITH_INTEGRATION,
+					"Unable to load classification nodes. Code: " + e.getCode() + ", Message: " + e.getMessage(), e);
+		}
+
+		WorkItemTypesFieldApi issueTypeFieldsApi = new WorkItemTypesFieldApi(defaultClient);
+		FieldsApi fieldsApi = new FieldsApi(defaultClient);
+        List<PostFormField> ticketFields = new ArrayList<>();
+		try {
+            List<WorkItemTypeFieldWithReferences> issueTypeFields = issueTypeFieldsApi
+                    .workItemTypesFieldList(organizationName, projectName, issueType, API_VERSION, EXPAND);
+
+            for (WorkItemTypeFieldWithReferences field : issueTypeFields) {
+				WorkItemField detailedField = null;
+                // Skip fields that return 404 on request
+				try {
+					detailedField = fieldsApi
+							.fieldsGet(organizationName, field.getReferenceName(), projectName, API_VERSION);
+				} catch (ApiException e) {
+					continue;
+				}
+
+                // Skip read-only fields
+				if (detailedField.isReadOnly()) {
+					continue;
+				}
+
+                List<AllowedValue> allowed = new ArrayList<>();
+				switch (detailedField.getName()) {
+                    // Skip Work Item Type field cause we have the same custom field
+					case "Work Item Type":
+						continue;
+					case "Iteration ID":
+						for (WorkItemClassificationNode node : iterationNodes) {
+							allowed.add(new AllowedValue(node.getId().toString(), node.getName()));
+						}
+						break;
+					case "Area ID":
+						for (WorkItemClassificationNode node : areaNodes) {
+							allowed.add(new AllowedValue(node.getId().toString(), node.getName()));
+						}
+						break;
+					default:
+						for (Object value : field.getAllowedValues()) {
+							allowed.add(new AllowedValue(value.toString().replace(" ", "_"), value.toString()));
+						}
+						break;
+				}
+
+                // Add an empty line to each field with a non-empty list of allowed values
+				if (allowed.size() > 0) {
+                    allowed.add(0, new AllowedValue("Empty_String", ""));
+                }
+
+				List<String> defaultValue = new ArrayList<>();
+				if (field.getDefaultValue() != null) {
+					defaultValue.add(field.getDefaultValue().toString().replace(" ", "_"));
+				}
+
+                ticketFields.add(new PostFormField(field.getReferenceName().replace(".", "_"), field.getName(),
+                        detailedField.getType().toString(), field.isAlwaysRequired(), defaultValue, allowed));
+			}
+
+            List<PostFormField> sortedTicketFields = ticketFields.stream()
+                    .sorted(Comparator.comparing(PostFormField::getIsRequired).reversed()
+                            .thenComparing(PostFormField::getFieldName)).collect(Collectors.toList());
+
+			// Add to the top a custom field representing the work item type
+			sortedTicketFields.add(0, new PostFormField("issuetype", "Issue Type", "issuetype", true,
+                    List.of(issueType), new ArrayList<AllowedValue>()));
+
+			return sortedTicketFields;
+		} catch (ApiException e) {
+            LOGGER.error("Unable to load ticket fields: " + e.getMessage(), e);
+			throw new ReportPortalException(ErrorType.UNABLE_INTERACT_WITH_INTEGRATION,
+					"Unable to load ticket fields. Code: " + e.getCode() + ", Message: " + e.getMessage(), e);
+		}
+	}
+
+	@Override
+	public List<String> getIssueTypes(Integration integration) {
+		Map<String, Object> params = integration.getParams().getParams();
+		String organizationUrl = params.get("url").toString();
+		String projectName = params.get("project").toString();
+		String personalAccessToken = params.get("oauthAccessKey").toString();
+
+		ApiClient defaultClient = getConfiguredApiClient(personalAccessToken);
+		String organizationName = organizationUrl.replace(defaultClient.getBasePath(), "");
+
+		WorkItemTypesApi issueTypesApi = new WorkItemTypesApi(defaultClient);
+		try {
+			List<WorkItemType> issueTypes = issueTypesApi.workItemTypesList(organizationName, projectName, API_VERSION);
+            return issueTypes.stream().map(WorkItemType::getName).collect(Collectors.toList());
+		} catch (ApiException e) {
+            LOGGER.error("Unable to load issue types: " + e.getMessage(), e);
+			throw new ReportPortalException(ErrorType.UNABLE_INTERACT_WITH_INTEGRATION,
+					"Unable to load issue types. Code: " + e.getCode() + ", Message: " + e.getMessage(), e);
+		}
+	}
+
+	private ApiClient getConfiguredApiClient(String personalAccessToken) {
+		ApiClient defaultClient = Configuration.getDefaultApiClient();
+		HttpBasicAuth basicAuth = (HttpBasicAuth) defaultClient.getAuthentication("accessToken");
+		basicAuth.setPassword(personalAccessToken);
+		return defaultClient;
+	}
+
+    private List<WorkItemClassificationNode> extractNestedNodes(WorkItemClassificationNode node) {
+        List<WorkItemClassificationNode> nodes = new ArrayList<>();
+        nodes.add(node);
+
+        if (node.isHasChildren()) {
+            for (WorkItemClassificationNode childrenNode : node.getChildren()) {
+                nodes.addAll(extractNestedNodes(childrenNode));
+            }
+        }
+        return nodes;
+    }
 }
