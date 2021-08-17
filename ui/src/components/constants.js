776b4549955742bb517353f3c64b6f6c8b4f697a
Alexandr Kislov <alexandr_kislov@epam.com>
2021-08-17 15:12:15 +0300

    Remove unused basic authorization

--- ui/src/components/constants.js
+++ ui/src/components/constants.js
@@ -1,6 +1,3 @@
 export const URL_ATTRIBUTE_KEY = 'url';
 export const PROJECT_ATTRIBUTE_KEY = 'project';
-export const USERNAME_ATTRIBUTE_KEY = 'username';
-export const PASSWORD_ATTRIBUTE_KEY = 'password';
 export const TOKEN_ATTRIBUTE_KEY = 'oauthAccessKey';
-export const CHECKBOX_ATTRIBUTE_KEY = 'postAllowed';
