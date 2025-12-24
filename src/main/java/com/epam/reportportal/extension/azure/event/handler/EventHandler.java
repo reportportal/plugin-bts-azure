1473dfb4ef9758aab8fe6a1ed4d68d4e13770c4d
Evelina Sarkisian <59954853+Evelina02@users.noreply.github.com>
2025-12-24 18:23:40 +0300

    Merge pull request #86 from reportportal/EPMRPP-109656-events-refactoring
    EPMRPP-109656 || Events refactoring

--- src/main/java/com/epam/reportportal/extension/azure/event/handler/EventHandler.java
+++
@@ -1,9 +0,0 @@
-package com.epam.reportportal.extension.azure.event.handler;
-
-/**
- * @author <a href="mailto:ivan_budayeu@epam.com">Ivan Budayeu</a>
- */
-public interface EventHandler<T> {
-
-	void handle(T event);
-}
